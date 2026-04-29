dbeeda89bceea5d94247d7768706491832c49bcc
SpadeXX-git <user.email>
2026-04-29 10:14:24 +0100

    removed darkmode

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-
-let lightmode = false;
-
-document.querySelector(".lightmode").addEventListener("click", function () {
-    lightmode = !lightmode;
-
-    if (lightmode) {
-        document.body.style.backgroundImage = "none";
-        document.body.style.color = "black";
-        document.querySelector(".header-text-text").style.color = "black";
-        document.querySelectorAll(".header, footer").forEach(item => {
-            item.style.backgroundColor = "white";
-        });
-        document.querySelectorAll(".logo, .headerlink, h5, .nav-item, .footer-text").forEach(item => {
-            item.style.color = "black";
-        });
-    } else {
-        document.body.style.backgroundImage = "";
-        document.body.style.color = "";
-        document.querySelector(".header-text-text").style.color = "";
-        document.querySelectorAll(".header, footer").forEach(item => {
-            item.style.backgroundColor = "";
-        });
-        document.querySelectorAll(".logo, .headerlink, h5, .nav-item, .footer-text").forEach(item => {
-            item.style.color = "";
-        });
-    }
-});
-
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.Tlinks');
 
